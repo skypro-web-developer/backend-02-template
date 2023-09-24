61c6bc6366b0d6c3560734e7f4981ab3c9e05467
PavelChuprin <pcuprin@gmil.com>
2023-09-24 16:20:54 +0600

    hw-2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-const http = require('http');
+const http = require("http");
+const getUsers = require("./modules/users");
+const hostname = "http://127.0.0.1";
+const port = 3003;
 
 const server = http.createServer((request, response) => {
+  const url = new URL(request.url, hostname);
+  const params = url.searchParams;
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+  // если параметры переданы
+  if (params.toString() !== "") {
+    // строка поиска не пуста
+    if (params.has("hello")) {
+      //есть ли в запросе параметр: "hello" (вернет Boolean)
+      const name = params.get("hello"); // возвращаем первое значение, связанное с заданным параметром поиска
+      if (name) {
+        response.status = 200;
+        response.header = "Content-Type: text/plain";
+        response.write(`Hello, ${name}.`);
+        response.end();
+        return;
+      } else {
+        response.status = 400;
+        response.header = "Content-Type: text/plain";
+        response.write("Enter a name");
+        response.end();
+        return;
+      }
+    } else if (params.has("users")) {
+      response.status = 200;
+      response.statusMessage = "OK";
+      response.header = "Content-Type: application/json";
+      response.write(getUsers());
+      response.end();
+      return;
+    } else {
+      response.status = 500;
+      response.header = "Content-Type: text/plain";
+      response.write("");
+      response.end();
+      return;
+    }
+  }
 
+  // никакие параметры не переданы
+  response.status = 200;
+  response.statusMessage = "OK";
+  response.header = "Content-Type: text/plain";
+  response.write("Hello, world!");
+  response.end();
+});
+
+server.listen(port, () => {
+  console.log(`Сервер запущен по адресу ${hostname}:${port}`);
 });
