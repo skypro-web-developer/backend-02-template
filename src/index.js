e52b121bdc088e9119602496681f591832ed7154
djon198360 <le1907@mail.ru>
2023-10-29 09:13:24 +0300

    fix error

--- src/index.js
+++ src/index.js
@@ -2,73 +2,54 @@ const http = require("http");
 const url = require("node:url");
 const getUsers = require("./modules/users");
 
-const hostname = "127.0.0.1";
-const port = 3003;
+const server = http.createServer(async (request, response) => {
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  const params = url.searchParams;
+  const name = params.get("hello");
 
-const server = http.createServer((req, res) => {
-  const myURL = new URL(req.url, "http://127.0.0.1");
-  const query = myURL.searchParams;
+  if (request.url === "/users") {
+    response.status = 200;
+    response.statusMessage = "ok";
+    response.header = "Content-Type: application/json";
+    response.write(getUsers());
+    response.end();
+  }
 
-  if (query.has("users")) {
-    res.statusCode = 200;
-    res.statusMessage = "OK";
-    res.setHeader("Content-Type", "application/json");
-    res.write = getUsers();
-    res.end(getUsers());
-    return;
-  } else if (query.has("hello")) {
-    if (query.get("hello")) {
-      const name = query.get("hello");
-      res.res.statusCode = 200;
-      res.statusMessage = "OK";
-      res.setHeader("Content-Type", "text/plain");
-      res.write = getUsers();
-      res.end(`Hello, ${name}`);
-      return;
+  if (params.has("hello")) {
+    if (name === "") {
+      response.statusCode = 400;
+      response.statusMessage = "Error";
+      (response.setHeader = "Content-Type"), "text/plain";
+      response.write("Enter a name");
+      response.end();
     } else {
-      res.statusCode = 400;
-      res.statusMessage = "OK";
-      res.setHeader("Content-Type", "text/plain");
-      res.write = getUsers();
-      res.end("Enter a name");
+      response.statusCode = 200;
+      response.statusMessage = "OK";
+      (response.setHeader = "Content-Type"), "text/plain";
+      response.write(`Hello,${name}`);
+      response.end();
       return;
     }
-  } else if (myURL.pathname === "/favicon.ico") {
-    res.statusCode = 200;
-    res.statusMessage = "OK";
-    res.setHeader("Content-Type", "image/x-icon");
-    res.write = "favicon.ico";
-    res.end("favicon.ico");
+  }
+
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    (response.setHeader = "Content-Type"), "text/plain";
+    response.write("Hello world");
+    response.end();
+
     return;
-  } else if (
-    !query.has("users") ||
-    !query.has("Hello") ||
-    !query.has("hello") ||
-    myURL.pathname !== "/favicon.ico"
-  ) {
-    res.statusCode = 500;
-    res.statusMessage = "OK";
-    res.setHeader("Content-Type", "text/plain");
-    res.write = "";
-    res.end(null);
-    return;
-  } else {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.write("Hello, World!");
-    res.end("Hello World\n");
+  }
+
+  if (!params.has("hello")) {
+    response.statusCode = 500;
+    response.statusMessage = "not OK";
+    (response.setHeader = "Content-Type"), "text/plain";
+    response.end();
   }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
+server.listen(3003, () => {
+  console.log("сервер успешно запущен на сервере 127.0.0.1:3003");
 });
-
-/* 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
-*/
