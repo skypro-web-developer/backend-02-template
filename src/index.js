50f38a5bbed197e602f55a226d0793c83e6a9597
DianaSemenova <menestrelia199702@gmail.com>
2023-11-18 11:48:46 +0400

    Создаy сервер на Node.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-const http = require('http');
+const http = require("http");
+const getUsers = require("./modules/users");
 
+const hostname = "127.0.0.1";
+const port = 3003;
+
+// создание сервера происходит при помощи функции createServer
 const server = http.createServer((request, response) => {
+  const url = new URL(request.url, `http://${hostname}`);
+  console.log(url);
+  console.log(url.searchParams);
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+  const query = url.searchParams;
 
+  if (query.has("hello")) {
+    const name = query.get("hello");
+
+    if (name) {
+      response.statusCode = 200;
+      response.setHeader = "Content-Type: text/plain";
+      response.write(`Hello, ${name}!`);
+      response.end();
+      return;
+    }
+
+    response.statusCode = 400;
+    response.setHeader = "Content-Type: text/plain";
+    response.write("Enter a name");
+    response.end();
+    return;
+  } else if (url.pathname === "/users") {
+    response.statusCode = 200;
+    response.setHeader = "Content-Type: application/json";
+    response.write(getUsers());
+    response.end();
+    return;
+  } else if (url.search === "") {
+    response.statusCode = 200;
+    response.setHeader = "Content-Type: text/plain";
+    response.write("Hello, World!");
+    response.end();
+  } else {
+    response.statusCode = 500;
+    response.end();
+    return;
+  }
+});
+
+// запуск сервера выполняется командой .listen
+server.listen(port, hostname, () => {
+  console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
 });
