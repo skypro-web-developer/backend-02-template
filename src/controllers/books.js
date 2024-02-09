6d46380491a27de78ecd75c97fb15057c1473412
Denis <den.sautner@mail.ru>
2024-02-09 14:39:54 +0600

    finished requests

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -2,33 +2,66 @@ const { request, response } = require("express");
 const Book = require("../models/book");
 
 const getBooks = (request, response) => {
-  return Book.find({}).then((books) => {
-    response.statusCode = 200;
-    response.send(books);
-  });
+  return Book.find({})
+    .then((books) => {
+      response.statusCode = 200;
+      response.send(books);
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 const getBook = (request, response) => {
   const { book_id } = request.params;
-  return Book.findById(book_id).then((book) => {
-    response.statusCode = 200;
-    response.send(book);
-  });
+  return Book.findById(book_id)
+    .then((book) => {
+      response.statusCode = 200;
+      response.send(book);
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 const createBook = (request, response) => {
-  return Book.create({ ...request.body }).then((book) => {
-    response.statusCode = 201;
-    response.send(book);
-  });
+  return Book.create({ ...request.body })
+    .then((book) => {
+      response.statusCode = 201;
+      response.send(book);
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 const updateBook = (request, response) => {
-  //Update book
+  const { book_id } = request.params;
+  return Book.findByIdAndUpdate(book_id, { ...request.body })
+    .then((book) => {
+      response.statusCode = 201;
+      response.send(book);
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 const deleteBook = (request, response) => {
-  //Delete book
+  const { book_id } = request.params;
+  return Book.findByIdAndDelete(book_id)
+    .then(() => {
+      response.statusCode = 201;
+      response.send("Книга удалена из списка");
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 module.exports = { getBooks, getBook, createBook, updateBook, deleteBook };
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,34 +2,66 @@ const { request, response } = require("express");
 const User = require("../models/user");
 
 const getUsers = (request, response) => {
-  return User.find({}).then((users) => {
-    response.statusCode = 200;
-    response.send(users);
-  });
+  return User.find({})
+    .then((users) => {
+      response.statusCode = 200;
+      response.send(users);
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 const getUser = (request, response) => {
   const { user_id } = request.params;
-  return User.findById(user_id).then((user) => {
-    response.statusCode = 200;
-    response.send(user);
-  });
+  return User.findById(user_id)
+    .then((user) => {
+      response.statusCode = 200;
+      response.send(user);
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 const createUser = (request, response) => {
-  return User.create({ ...request.body }).then((user) => {
-    response.statusCode = 201;
-    response.send(user);
-  });
+  return User.create({ ...request.body })
+    .then((user) => {
+      response.statusCode = 201;
+      response.send(user);
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 const updateUser = (request, response) => {
-  response.statusCode = 201;
-  response.send(request);
+  const { user_id } = request.params;
+  return User.findByIdAndUpdate(user_id, { ...request.body })
+    .then((user) => {
+      response.statusCode = 201;
+      response.send(user);
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 const deleteUser = (request, response) => {
-  //Delete user
+  const { user_id } = request.params;
+  return User.findByIdAndDelete(user_id)
+    .then(() => {
+      response.statusCode = 201;
+      response.send("Пользователь удален из списка");
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.send(error.message);
+    });
 };
 
 module.exports = { getUsers, getUser, createUser, updateUser, deleteUser };
