a2e63bfbe4ae600ccce07e2b47cbeb34127f9933
Alexgbp <bboy91@bk.ru>
2024-03-12 05:58:19 +0300

    env

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const app = express()
 const userRouter = require("./routers/userRouter")
 
-const {port = 3005, url = "127.0.0.1"} = process.env
+dotenv.config()
+const {port = 3005, url = "127.0.0.1", mongo_url = "mongodb://localhost:27017/mydb"} = process.env
 
-mongoose.connect("mongodb://localhost:27017/mydb")
+mongoose.connect(mongo_url)
 .then(() => console.log("Подключение к БД"))
 .catch((err) => console.log(err.message))
 
