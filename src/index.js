e67f55f3154d30fd17717aff03c01a61afbef719
Alexgbp <bboy91@bk.ru>
2024-03-12 05:11:47 +0300

    mongoose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 const app = express()
 const userRouter = require("./routers/userRouter")
 
 const {port = 3005, url = "127.0.0.1"} = process.env
+
+mongoose.connect("mongodb://localhost:27017/mydb")
+.then(() => console.log("Подключение к БД"))
+.catch((err) => console.log(err.message))
+
 
 app.use(userRouter)
 app.use(bodyParser.json())
