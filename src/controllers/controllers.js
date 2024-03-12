7b12ed37ca169132753536b1836026361c0c718d
Alexgbp <bboy91@bk.ru>
2024-03-12 21:03:24 +0300

    del user

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -9,5 +9,10 @@ const addUsers = (req, res) => {
     User.create(data)
     .then((user)=> {res.status(201).send(user)})
 }
+const delUser = (req, res) => {
+    const {user_id} = req.params
+    User.findByIdAndDelete(user_id)
+    .then((user)=> {res.status(200).send(user)})
+}
 
-module.exports = {getUsers, addUsers}
+module.exports = {getUsers, addUsers, delUser}
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const {getUsers, addUsers} = require("../controllers/controllers")
+const {getUsers, addUsers, delUser} = require("../controllers/controllers")
 const logerUrl = require("../middleware/loger")
 
 
@@ -7,5 +7,6 @@ router.use(logerUrl)
 
 router.get("/users", getUsers)
 router.post("/users", addUsers)
+router.delete("/users/:user_id", delUser)
 
 module.exports = router
