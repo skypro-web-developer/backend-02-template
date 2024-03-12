604938de106ee43b5dfab3b2b21a31ba3d956cad
Alexgbp <bboy91@bk.ru>
2024-03-12 23:26:15 +0300

    add valiadation

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -3,33 +3,57 @@ const User = require("../models/userModels")
 const getUsers = (req, res) => {
     User.find({})
     .then((user) => {res.status(200).send(user)})
+    .catch((err)=> {
+    res.status(404).send(err.message)
+    res.status(500).send(err.message);
+    })
 }
 const addUsers = (req, res) => {
     const data = req.body
     User.create(data)
     .then((user)=> {res.status(201).send(user)})
+    .catch((err)=> {
+    res.status(404).send(err.message)
+	res.status(500).send(err.message);
+    })
 }
 const delUser = (req, res) => {
     const {user_id} = req.params
     User.findByIdAndDelete(user_id)
-    .then((user)=> {res.status(200).send(user)})
+    .then((user)=> {
+        if (!user) {
+            res.status(404);
+          } else {
+            res.status(200).send(user);
+          }
+    })
+    .catch((err)=> res.status(500).send(err.message))
 }
 const getUser = (req, res) => {
     const {user_id} = req.params
     User.findById(user_id)
-    .then((user)=> {res.status(200).send(user)})
+    .then((user)=> {
+        if (!user) {
+            res.status(404);
+          } else {
+            res.status(200).send(user);
+          }
+        })
+    .catch((err)=> res.status(500).send(err.message))
 }
+
 const updateUser = (req, res) => {
     const data = req.body
     const {user_id} = req.params
     User.findByIdAndUpdate(user_id, data)
-    .then((user)=> {res.status(200).send(user)})
+    .then((user)=> {
+        if (!user) {
+            res.status(404);
+          } else {
+            res.status(200).send(user);
+          }
+        })
+    .catch((err)=> res.status(500).send(err.message))
 }
-const getBooksUserById = (req, res) => {
-  const { user_id } = req.params;
-  User.findById(user_id).then((user) => {
-    res.status(200).send(user.books);
-  });
-};
 
-module.exports = {getUsers, addUsers, delUser, getUser,updateUser, getBooksUserById}
+module.exports = {getUsers, addUsers, delUser, getUser, updateUser}
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const {getUsers, addUsers, delUser, getUser, updateUser, getBooksUserById} = require("../controllers/controllers")
+const {getUsers, addUsers, delUser, getUser, updateUser} = require("../controllers/controllers")
 const logerUrl = require("../middleware/loger")
 
 
@@ -7,7 +7,6 @@ router.use(logerUrl)
 
 router.get("/users", getUsers)
 router.get("/users/:user_id", getUser)
-router.get("/users/:user_id/books", getBooksUserById)
 router.post("/users", addUsers)
 router.patch("/users/:user_id", updateUser)
 router.delete("/users/:user_id", delUser)
