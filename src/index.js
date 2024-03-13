0551e77fefa65feeefff494306cb3f5eb86bfdc0
Alexgbp <bboy91@bk.ru>
2024-03-13 23:12:45 +0300

    check

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const app = express()
-const userRouter = require("./routers/userRouter")
-const bookRouter = require("./routers/bookRouter")
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const userRouter = require("./routers/userRouter");
+const bookRouter = require("./routers/bookRouter");
 
-dotenv.config()
-const {port = 3005, url = "127.0.0.1", mongo_url = "mongodb://localhost:27017/mydb"} = process.env
+dotenv.config();
+const {
+  port = 3005,
+  url = "127.0.0.1",
+  mongo_url = "mongodb://localhost:27017/mydb",
+} = process.env;
 
-mongoose.connect(mongo_url)
-.then(() => console.log("Подключение к БД"))
-.catch((err) => console.log(err.message))
+mongoose
+  .connect(mongo_url)
+  .then(() => console.log("Подключение к БД"))
+  .catch((err) => console.log(err.message));
 
-
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use(userRouter)
-app.use(bookRouter)
-app.listen(port, () => console.log(`Сервер запущен на ${url}:${port}`))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(userRouter);
+app.use(bookRouter);
+app.listen(port, () => console.log(`Сервер запущен на ${url}:${port}`));
