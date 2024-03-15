adad97805c1d33a56d2515bfdba5a99a50b9adab
Vera869 <verea8910@gmail.com>
2024-03-15 21:43:26 +0700

    +

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// const http = require('http');
-
-// const server = http.createServer((request, response) => {
 
 //     // Написать обработчик запроса:
 //     // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
@@ -8,8 +5,6 @@
 //     // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
 //     // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
 //     // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
-
-// });
 
 const http = require("http");
 const hostname = "127.0.0.1";
@@ -21,48 +16,50 @@ const server = http.createServer((request, response) => {
   const url = new URL(request.url, ipAddress);
   const userName = url.searchParams.get("hello");
   if (userName) {
-    response.status = 200;
+    response.statusCode = 200;
     response.statusMessage = "ok";
     response.setHeader("Content-Type", "text/plain");
     response.write(`Hello, ${userName}`);
     response.end();
     return;
-  }
+  }else {
+ 
   switch (request.url) {
     case "/users":
-      response.status = 200;
+      response.statusCode = 200;
       response.statusMessage = "OK";
       response.setHeader("Content-Type", "application/json");
       response.write(getUsers());
       response.end();
       break;
 
-    case "/?hello":
-      response.status = 400;
-      response.statusMessage = "Bad Request";
-      response.setHeader("Content-Type", "text/plain");
-      response.write(`Enter a name`);
-      response.end();
-      break;
-
     case "/":
-      response.status = 200;
+      response.statusCode = 200;
       response.statusMessage = "OK";
       response.setHeader("Content-Type", "text/plain");
       response.write(`Hello, world`);
       response.end();
       break;
 
+    case "/?hello":
+      response.statusCode = 400;
+      response.statusMessage = "Bad Request";
+      response.setHeader("Content-Type", "text/plain");
+      response.write(`Enter a name`);
+      response.end();
+      break;
+
     default:
-      response.status = 500;
+      response.statusCode = 500;
       response.statusMessage = "Server Error";
       response.setHeader("Content-Type", "text/plain");
       response.write(""
-        //"error500"
+        // "error500"
         );
       response.end();
       break;
   }
+}
 });
 
 server.listen(port, hostname, () => {
